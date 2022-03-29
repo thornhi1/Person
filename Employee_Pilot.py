3c80a9ac8c9d18d4568a3dc1d851af0a5b229fdf
thornhi1 <44597185+thornhi1@users.noreply.github.com>
2022-03-28 22:01:12 -0600

    commit

--- Employee_Pilot.py
+++ Employee_Pilot.py
@@ -6,16 +6,16 @@ from PIL import Image
 
 
 # Data needed for the app
-data_stage = pd.read_csv('C:/Users/jefft/OneDrive/Documents/Python Scripts/Streamlit/Employee_Example_Data.csv')
+data_stage = pd.read_csv('Employee_Example_Data_Generic.csv')
 data = pd.DataFrame(data_stage)
 
 # Main body of the application.
 st.title('Employee Locator')
-st.subheader('Find Health Catalyst team members in your area to connect with.')
+st.subheader('Find Company team members in your area to connect with.')
 
 
 # Filters on the left.
-image = Image.open('C:/Users/jefft/OneDrive/Documents/Python Scripts/Streamlit/Company_Logo.jpg')
+#image = Image.open('Company_Logo.jpg')
 #st.sidebar.image(image,width=100)
 st.sidebar.header('Company')
 st.sidebar.text('Filters')
@@ -45,35 +45,50 @@ HQ_stage = data[data['Location'] == 'US SLC Headquarters']
 HQ = (HQ_stage['Team Member ID'].count())*1.0
 
 col1, col2, col3 = st.columns(3)
-col1.metric("Employee Count", employee_count)
-col2.metric("Remote", "{:.2%}".format(remote_pct)) #remote_pct
-col3.metric("Size of Headquarters", HQ)
+
+with col1:
+    st.metric("Employee Count", employee_count)
+
+with col2:
+    st.metric("Remote", "{:.2%}".format(remote_pct)) #remote_pct
+
+with col3:
+    st.metric("Size of Headquarters", HQ)
 
 
 st.subheader('Map of Employees')
 st.map(filtered_data)
 
-bar_chart = data['State'].value_counts()
-bar_chart = pd.DataFrame(bar_chart)
-bar_chart = bar_chart.reset_index()
+col4, col5 = st.columns(2)
+
+with col4:
+    bar_chart = data['State'].value_counts()
+    bar_chart = pd.DataFrame(bar_chart)
+    bar_chart = bar_chart.reset_index()
 
 
-bars = alt.Chart(bar_chart).mark_bar().encode(
-    x=alt.X("State",axis=None), #alt.Axis(grid=False)),
-    y=alt.Y("index", sort='-x',axis=alt.Axis(grid=False)),
-    color=alt.condition(
-        alt.datum.index == bar_chart['index'].max(),  # If the year is 1810 this test returns True,
-        alt.value('blue'),     # which sets the bar orange.
-        alt.value('steelblue')   # And if it's not true it sets the bar steelblue.
+    bars = alt.Chart(bar_chart).mark_bar().encode(
+        x=alt.X("State",axis=None), #alt.Axis(grid=False)),
+        y=alt.Y("index", sort='-x',axis=alt.Axis(grid=False)),
+        color=alt.condition(
+            alt.datum.index == bar_chart['index'].max(),  # If the year is 1810 this test returns True,
+            alt.value('blue'),     # which sets the bar orange.
+            alt.value('steelblue')   # And if it's not true it sets the bar steelblue.
+        )
     )
-)
 
-text = bars.mark_text(
-    align='left',
-    baseline='middle',
-    dx=3  # Nudges text to right so it doesn't appear on top of the bar
-).encode(
-    text='State:Q'
-)
+    text = bars.mark_text(
+        align='left',
+        baseline='middle',
+        dx=3  # Nudges text to right so it doesn't appear on top of the bar
+    ).encode(
+        text='State:Q'
+    )
 
-bars + text
+    bars + text
+
+with col5:
+    employee_data = data.loc[(data['State'] == state_choice) & (data['Home Zip Code'] == zip_choice)]
+    employee_data = employee_data[['Full Name',' Email']]
+
+    st.dataframe(employee_data)
